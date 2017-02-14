51906c7009a3ff313af5125aa55b20c133b736f5
Kofifus <mikagenic@gmail.com>
2017-02-14 19:57:09 +1100

    Update new.js

--- new.js
+++ new.js
@@ -1,31 +1,19 @@
 // https://github.com/kofifus/New.js
 if (typeof Function.prototype.New === 'undefined') {
-	Function.prototype.New= function(...args) {
-		let isDict = d => (d !==undefined && d!==null && typeof d==='object' && d.constructor!==Array && d.constructor!==Date); 
+	Function.prototype.New=function(...args) {
+    // not using object.assign so we can copy get/set
+    function copyProps(to, from) { Object.getOwnPropertyNames(from).forEach(key => { if (!to.hasOwnProperty(key)) Object.defineProperty(to, key, Object.getOwnPropertyDescriptor(from, key)); }); }
 
-		// get public interface
-		let header=this();
-		if (!isDict(header)) throw 'New - invalid interface';
+		// create header 
+		let header={};
 		Object.setPrototypeOf(header, this.prototype); // fix prototype for instanceof
+		copyProps(header, this.call(header, ...args));
 		
-		// ctor
-		let ctor=header.ctor, composed;
-		if (ctor && typeof ctor!=='function') throw 'New - invalid ctor';
-		if (args.length>0 && !ctor) throw('New - missing ctor'); // no ctor to send arguments
-		if (ctor) {
-			composed=ctor.call(header, ...args);
-			delete header.ctor; // remove ctor from interface
-		}
-
-		// compose
-		if (composed) {
-			if (isDict(composed)) composed=[composed];
-			if (composed.constructor!==Array) throw 'New - invalid composition';
-			composed.forEach(a => {
-				if (!isDict(a)) throw 'New - invalid composition';
-				let props = Object.getOwnPropertyNames(a);
-				props.forEach(key => { if (!header.hasOwnProperty(key)) Object.defineProperty(header, key, Object.getOwnPropertyDescriptor(a, key)); });
-			});
+		// fill instance with composed properties
+		if (header.composed) {
+		  if (!Array.isArray(header.composed)) header.composed=[header.composed];
+		  header.composed.forEach(props => copyProps(header, props));
+		  delete header.composed;
 		}
 
 		return header;
