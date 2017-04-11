a1c2a5acab9180288c341f3550421412a19ae2da
Kofifus <mikagenic@gmail.com>
2017-04-11 14:31:32 +1000

    Update new.js

--- new.js
+++ new.js
@@ -1,25 +1,20 @@
 // https://github.com/kofifus/New.js
 if (typeof Function.prototype.New === 'undefined') {
-  Function.prototype.New = function(...args) {
-    function copyProps(to, from) {
-      // not using object.assign so we can copy get/set
-      Object.getOwnPropertyNames(from).forEach(key => {
-        if (!to.hasOwnProperty(key)) Object.defineProperty(to, key, Object.getOwnPropertyDescriptor(from, key));
-      });
-    }
+	Function.prototype.New=function(...args) {
+    // not using object.assign so we can copy get/set
+    function copyProps(to, from) { Object.getOwnPropertyNames(from).forEach(key => { if (!to.hasOwnProperty(key)) Object.defineProperty(to, key, Object.getOwnPropertyDescriptor(from, key)); }); }
 
-    // create header 
-    let header = {};
-    Object.setPrototypeOf(header, this.prototype); // fix prototype for instanceof
-    copyProps(header, this.call(header, ...args));
+		// create header 
+		let header=Object.create(this.prototype);
+		copyProps(header, this.call(header, ...args));
+		
+		// fill instance with composed properties
+		if (header.composed) {
+		  if (!Array.isArray(header.composed)) header.composed=[header.composed];
+		  header.composed.forEach(props => copyProps(header, props));
+		  delete header.composed;
+		}
 
-    // fill instance with composed properties
-    if (header.composed) {
-      if (!Array.isArray(header.composed)) header.composed = [header.composed];
-      header.composed.forEach(props => copyProps(header, props));
-      delete header.composed;
-    }
-
-    return header;
-  }
+		return header;
+	}
 }
