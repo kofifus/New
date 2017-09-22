48f9630236154865bcbacd148ba98bf308791fd6
Kofifus <mikagenic@gmail.com>
2017-09-23 08:39:30 +1000

    Update new.js

--- new.js
+++ new.js
@@ -1,20 +1,20 @@
 // https://github.com/kofifus/New.js
 if (typeof Function.prototype.New === 'undefined') {
-	Function.prototype.New=function(...args) {
+  Function.prototype.New=function(...args) {
     // not using object.assign so we can copy get/set
     function copyProps(to, from) { Object.getOwnPropertyNames(from).forEach(key => { if (!to.hasOwnProperty(key)) Object.defineProperty(to, key, Object.getOwnPropertyDescriptor(from, key)); }); }
 
-		// create header 
-		let header=Object.create(this.prototype);
-		copyProps(header, this.call(header, ...args));
-		
-		// fill instance with composed properties
-		if (header.composed) {
-		  if (!Array.isArray(header.composed)) header.composed=[header.composed];
-		  header.composed.forEach(props => copyProps(header, props));
-		  delete header.composed;
-		}
+    // create header 
+    let header=Object.create(this.prototype);
+    copyProps(header, this.call(header, ...args));
 
-		return header;
-	}
+    // fill instance with composed properties
+    if (header.composed) {
+      if (!Array.isArray(header.composed)) header.composed=[header.composed];
+      header.composed.forEach(props => copyProps(header, props));
+      delete header.composed;
+    }
+
+    return header;
+  }
 }
